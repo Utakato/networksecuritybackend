f93670f34f4ebb4b3e6b790ef2910266eedefd24
Server <schiporvasile.u@gmail.com>
2025-06-01 13:16:07 +0300

    Update solana path

--- fetch_metadata.sh
+++ fetch_metadata.sh
@@ -1,1 +1,2 @@
+export PATH=/root/.local/share/solana/install/active_release/bin
 solana validator-info get --output json > /root/vasile/networksecuritybackend/validator_info_service/validator_info.json
